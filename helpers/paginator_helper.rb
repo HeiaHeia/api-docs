81c3f8e29716c5fcb8068f2d6779ee7cdd735c42
Kirill Nikitin <locke23rus@gmail.com>
2013-06-28 13:10:11 +0400

    Added helpers for params page and per_page.

--- helpers/paginator_helper.rb
+++ helpers/paginator_helper.rb
@@ -2,4 +2,28 @@
 
 module PaginatorHelper
 
+  def param_page
+    {
+      :name => :"page",
+      :paramType => "query",
+      :dataType => "byte",
+      :defaultValue => 1
+    }.to_json
+  end
+
+  def param_per_page
+    {
+      :name => "per_page",
+      :description => "Per page between 1 and 100",
+      :paramType => "query",
+      :dataType => "byte",
+      :defaultValue => "20",
+      :allowableValues =>  {
+        :valueType =>  "RANGE",
+        :min =>  1,
+        :max =>  100
+      }
+    }.to_json
+  end
+
 end
