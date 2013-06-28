1fc06733a0f52b612fa335ee5da7a8e963a94eed
Kirill Nikitin <locke23rus@gmail.com>
2013-06-28 13:16:23 +0400

    Added helper embedded_user_class.

--- helpers/models_helper.rb
+++ helpers/models_helper.rb
@@ -3,7 +3,29 @@
 module ModelsHelper
 
   def embedded_user_class
-
+    {
+      :id => "User",
+      :properties => {
+        :id => {
+          :type => "long",
+          :required => true
+        },
+        :first_name => {
+          :type => "string"
+        },
+        :last_name => {
+          :type => "string"
+        },
+        :avatar_url => {
+          :type => "string",
+          :required => true
+        },
+        :url => {
+          :type => "string",
+          :required => true
+        }
+      }
+    }.to_json
   end
 
 end
